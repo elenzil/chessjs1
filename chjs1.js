752317bcdc824e0b995101925e9feee2e48f212a
Orion Elenzil <oelenzil@microsoft.com>
2020-10-25 18:13:00 -0700

    ugh

--- chjs1.js
+++ chjs1.js
@@ -1,11 +1,46 @@
 
-var CHESS_OPTS = {
-    "viewAsWhite" : true,
+var gChess = {
+    options: {
+        viewAsWhite : true,
+    },
+
+    allFENCodes: ['p', 'r', 'n', 'b', 'q', 'k', 'P', 'R', 'N', 'B', 'Q', 'K'],
+
+    pieces: [],
 }
 
+function gebid(id) {
+    return document.getElementById(id);
+}
+
+function InitializeStandardChessPieces(containerElement) {
+    const pieces = gChess.pieces = [];
+
+    const keys = ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',
+                  'r', 'r', 'n', 'n', 'b', 'b', 'q', 'k',
+                  'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',
+                  'R', 'R', 'N', 'N', 'B', 'B', 'Q', 'K',];
+
+    for (var n = 0; n < keys.length; ++n) {
+        const key = keys[n];
+        const el = document.createElement("img");
+        el.className = 'piece';
+        el.src = GetImagePathForFENCode(key);
+        containerElement.appendChild(el);
+
+        const piece = {
+            "FEN": key,
+            "element": el,
+            "taken": true,
+            "dest": [n * 50, n * 50],
+        }
+
+        gChess.pieces[gChess.pieces.length] = piece;
+    }
+}
 
 function GetPositionName(oneBasedRow, oneBasedCol) {
-    if (!CHESS_OPTS.viewAsWhite) {
+    if (!gChess.options.viewAsWhite) {
         oneBasedRow = 9 - oneBasedRow;
         oneBasedCol = 9 - oneBasedCol;
     }
@@ -75,18 +110,63 @@ function SetBoardStateFromFEN(FEN) {
             col += 1;
         }
     }
+}
 
+function GetNumberOfTakenPieces() {
+    var ret = 0;
+    gChess.pieces.array.forEach(element => {
+        if (element.isTaken) {
+            ret += 1;
+        }
+    });
+    return ret;
+}
+
+function GetPieceBoxCellId(index) {
+    "pieceBoxCell_" + n;
+}
+
+function GetPieceBoxCell(index) {
+    return gebid(GetPieceBoxCellId(n));
+}
+
+function GetNextAvailablePieceBoxCell() {
+    return GetPieceBoxCell(GetNumberOfTakenPieces() % (8 * 3));
 }
 
 function ResetBoardToStockBeginning() {
-    SetBoardStateFromFEN('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR');
+    InitializeStandardChessPieces(gebid('piecesLayer'));
+  //  SetBoardStateFromFEN('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR');
 }
 
 function OnLoad() {
     ResetBoardToStockBeginning();
+    window.requestAnimationFrame(Tick);
 }
 
 function FlipBoard() {
-    CHESS_OPTS.viewAsWhite = !CHESS_OPTS.viewAsWhite;
+    gChess.options.viewAsWhite = !gChess.options.viewAsWhite;
     ResetBoardToStockBeginning();
+}
+
+let FirstTickTime;
+function Tick(timestamp) {
+
+    timestamp /= 1000;
+
+    if (FirstTickTime === undefined) {
+        FirstTickTime = timestamp;
+    }
+
+    const dt = timestamp - FirstTickTime;
+
+    for (var n = 0; n < gChess.pieces.length; ++n) {
+        const piece = gChess.pieces[n];
+        const el    = piece.element;
+        const rect = el.getBoundingClientRect();
+        el.style.left = (Math.cos(n + dt / 2 * (1 + n/22.1)) * 0.5 + 0.5) * 640;
+        el.style.top  = (Math.sin(n + dt / 2 * (1 + n/12.1)) * 0.5 + 0.5) * 640;
+    }
+
+    window.requestAnimationFrame(Tick);
 }
